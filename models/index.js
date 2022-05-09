6eaf4e251d3c3d4fb6b7fa146f0b03d7e643d661
Charanvir <charanvir123@gmail.com>
2022-05-08 23:10:37 -0400

    created get all route for products to include categories and tags, using the through product tag route

--- models/index.js
+++ models/index.js
@@ -15,13 +15,13 @@ Category.hasMany(Product, {
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
-  as: 'producttag',
+  as: 'tags',
   foreignKey: 'product_id'
 })
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
-  as: 'producttag',
+  as: 'products',
   foreignKey: 'tag_id'
 })
 
